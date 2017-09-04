1146140b0930a32b04c8af78a496c9dcee05881c
KevinVi <kevin.vivor@gmail.com>
2017-09-04 20:05:22 +0200

    add profiles

--- src/main/java/com/example/cinemaplanner/CinemaPlannerApplication.java
+++ src/main/java/com/example/cinemaplanner/CinemaPlannerApplication.java
@@ -2,11 +2,18 @@ package com.example.cinemaplanner;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 @SpringBootApplication
 public class CinemaPlannerApplication {
+    // extends SpringBootServletInitializer
+//    @Override
+//    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+//        return application.sources(CinemaPlannerApplication.class);
+//    }
 
-	public static void main(String[] args) {
-		SpringApplication.run(CinemaPlannerApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(CinemaPlannerApplication.class, args);
+    }
 }
